cbdfa240cbede7460220f3684e6fb1d169517077
ItayMadeIt <itayna73@gmail.com>
2025-05-13 20:21:42 +0300

    Made GDT into it's own file

---
+++ kernel/kernel/gdt.c
@@ -0,0 +1,142 @@
+#include <stdint.h>
+ 
+// Each define here is for a specific flag in the descriptor.
+// Refer to the intel documentation for a description of what each one does.
+#define SEG_DESCTYPE(x)  ((x) << 0x04) // Descriptor type (0 for system, 1 for code/data)
+#define SEG_PRES(x)      ((x) << 0x07) // Present
+#define SEG_SAVL(x)      ((x) << 0x0C) // Available for system use
+#define SEG_LONG(x)      ((x) << 0x0D) // Long mode
+#define SEG_SIZE(x)      ((x) << 0x0E) // Size (0 for 16-bit, 1 for 32)
+#define SEG_GRAN(x)      ((x) << 0x0F) // Granularity (0 for 1B - 1MB, 1 for 4KB - 4GB)
+#define SEG_PRIV(x)     (((x) &  0x03) << 0x05)   // Set privilege level (0 - 3)
+ 
+#define SEG_DATA_RD        0x00 // Read-Only
+#define SEG_DATA_RDA       0x01 // Read-Only, accessed
+#define SEG_DATA_RDWR      0x02 // Read/Write
+#define SEG_DATA_RDWRA     0x03 // Read/Write, accessed
+#define SEG_DATA_RDEXPD    0x04 // Read-Only, expand-down
+#define SEG_DATA_RDEXPDA   0x05 // Read-Only, expand-down, accessed
+#define SEG_DATA_RDWREXPD  0x06 // Read/Write, expand-down
+#define SEG_DATA_RDWREXPDA 0x07 // Read/Write, expand-down, accessed
+#define SEG_CODE_EX        0x08 // Execute-Only
+#define SEG_CODE_EXA       0x09 // Execute-Only, accessed
+#define SEG_CODE_EXRD      0x0A // Execute/Read
+#define SEG_CODE_EXRDA     0x0B // Execute/Read, accessed
+#define SEG_CODE_EXC       0x0C // Execute-Only, conforming
+#define SEG_CODE_EXCA      0x0D // Execute-Only, conforming, accessed
+#define SEG_CODE_EXRDC     0x0E // Execute/Read, conforming
+#define SEG_CODE_EXRDCA    0x0F // Execute/Read, conforming, accessed
+ 
+#define GDT_CODE_PL0 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
+                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
+                     SEG_PRIV(0)     | SEG_CODE_EXRD
+ 
+#define GDT_DATA_PL0 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
+                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
+                     SEG_PRIV(0)     | SEG_DATA_RDWR
+ 
+#define GDT_CODE_PL3 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
+                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
+                     SEG_PRIV(3)     | SEG_CODE_EXRD
+ 
+#define GDT_DATA_PL3 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
+                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
+                     SEG_PRIV(3)     | SEG_DATA_RDWR
+
+#define DESCRIPTORS_AMOUNT 5
+
+
+ #include <stdbool.h>
+
+typedef struct virtual_metadata
+{
+    uint32_t virtual_addr;  // 0xC0000000
+    uint32_t virtual_dir_index; // 768 -> 0xC0000000
+    uint32_t virtual_table_index; // 15 -> 0xC0015000
+    uint32_t physical_addr; // 0x00100032
+    uint32_t pages_amount;  // 2048 -> 4096*2048 bytes
+    uint16_t tail_12_bits;
+} virtual_metadata_t;
+
+typedef uint32_t page_entry;
+typedef uint32_t page_table_entry;
+
+#define ENTRIES_AMOUNT 1024
+
+typedef struct page_table
+{
+    page_entry entries[ENTRIES_AMOUNT];
+} page_table_t;
+
+typedef struct page_directory
+{
+    page_table_entry entries[ENTRIES_AMOUNT];
+} page_directory_t;
+
+
+void debug_print(uint32_t value)
+{
+    volatile static char* vga = (volatile char*)0xB8000;
+    vga+=sizeof(uint32_t) * 4 - 1;
+
+    uint8_t it = 8;
+
+    while (it)
+    {
+        char ch = (value & 0xF);
+        if (ch >= 10)
+        {
+            ch += 'A' - 10;
+        }
+        else 
+        {
+            ch += '0';
+        }
+
+        *vga-- = 0x0F;
+        *vga-- = ch;
+
+        value >>= 4;
+        --it;
+    }
+    vga += 80 * 2 + 1;
+}
+
+uint64_t create_descriptor(uint32_t base, uint32_t limit, uint16_t flag)
+{
+    uint64_t descriptor;
+ 
+    // Create the high 32 bit segment
+    descriptor  =  limit       & 0x000F0000;         // set limit bits 19:16
+    descriptor |= (flag <<  8) & 0x00F0FF00;         // set type, p, dpl, s, g, d/b, l and avl fields
+    descriptor |= (base >> 16) & 0x000000FF;         // set base bits 23:16
+    descriptor |=  base        & 0xFF000000;         // set base bits 31:24
+ 
+    // Shift by 32 to allow for low part of segment
+    descriptor <<= 32;
+ 
+    // Create the low 32 bit segment
+    descriptor |= base  << 16;                       // set base bits 15:0
+    descriptor |= limit  & 0x0000FFFF;               // set limit bits 15:0
+
+    return descriptor;
+}
+
+void setup_gdt(uint32_t gdt_addr)
+{
+    uint64_t descriptors[DESCRIPTORS_AMOUNT];
+    uint8_t i = 0;
+
+    descriptors[i++] = create_descriptor(0, 0, 0);
+    descriptors[i++] = create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL0));
+    descriptors[i++] = create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL0));
+    descriptors[i++] = create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL3));
+    descriptors[i++] = create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL3));
+
+    uint32_t* addr = (uint32_t*)gdt_addr;
+    for (i = 0; i < DESCRIPTORS_AMOUNT; ++i) 
+    {
+        addr[i * 2] = descriptors[i] & 0xFFFFFFFF;
+        addr[i * 2 + 1] = descriptors[i] >> 32;
+    }
+}
