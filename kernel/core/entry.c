6749c36514508152e2043fb8e6d197fa31dd4477
ItayMadeIt <itayna73@gmail.com>
2025-05-22 13:55:39 +0300

    Added timer.c for clarity in entry.c

--- kernel/core/entry.c
+++ kernel/core/entry.c
@@ -26,23 +26,6 @@ void virt_kernel_main()
     );
 }
 
-void IRQ0_handler();
-
-void setup_pit()
-{
-    set_idt_entry(0x20, IRQ0_handler, SEGMENT_SELECTOR_CODE_DPL0, IDT_INTERRUPT_32_DPL0);
-
-    const uint32_t frequency = 1000; // 1000 Hz
-
-    asm volatile (
-        "mov %0, %%ebx\n\r"
-        "call setup_pit_asm\n\r"
-        :
-        : "r" (frequency)
-        : "memory"
-    );
-}
-
 static inline void sti()
 {
     asm volatile("sti");
---
+++ kernel/core/timer.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <core/idt.h>
+
+void IRQ0_handler();
+
+void setup_pit()
+{
+    set_idt_entry(0x20, IRQ0_handler, SEGMENT_SELECTOR_CODE_DPL0, IDT_INTERRUPT_32_DPL0);
+
+    const uint32_t frequency = 1000; // 1000 Hz
+
+    asm volatile (
+        "mov %0, %%ebx\n\r"
+        "call setup_pit_asm\n\r"
+        :
+        : "r" (frequency)
+        : "memory"
+    );
+}
